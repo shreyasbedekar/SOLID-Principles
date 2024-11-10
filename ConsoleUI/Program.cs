cdd67b48c38ea987085f65abb52511f37f8aea87
Shreyas Bedekar <bedesn01@pfw.edu>
2024-11-09 23:16:20 -0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Name Generator!");
+            Person user = new Person();
+
+            Console.WriteLine("Enter your first name:");
+            user.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name:");
+            user.LastName = Console.ReadLine();
+
+            if(string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                Console.WriteLine("You must enter a valid first name.");
+                Console.ReadLine();
+                return;
+            }
+            if(string.IsNullOrWhiteSpace(user.LastName))
+            {
+                Console.WriteLine("You must enter a valid last name.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine($"Your username is: {user.FirstName.Substring(0, 1)}{user.LastName}");
+            Console.ReadLine();
+        }
+    }
+}
