65d3d92d16e29f9f8075af54d1d25fca7c6fe61d
Shreyas Bedekar <bedesn01@pfw.edu>
2024-11-12 22:45:59 -0500

    OCP

---
+++ OCPDemo/OCPLibrary/Applicants/IApplicantModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OCPLibrary
+{
+    public interface IApplicantModel
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        IAccounts AccountsProcessor { get; set; }
+    }
+}
