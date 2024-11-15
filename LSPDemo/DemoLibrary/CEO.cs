74047047108fc1becd6f9cf51b9bdc2b8839b599
Shreyas Bedekar <bedesn01@pfw.edu>
2024-11-14 19:08:40 -0500

    LSP

---
+++ LSPDemo/DemoLibrary/CEO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class CEO : BaseEmployee, IManager
+    {
+        public override void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 150m;
+
+            Salary = baseAmount * rank;
+        }
+
+        public void GeneratePerformanceReview()
+        {
+            Console.WriteLine("I'm reviewing a direct report's performance.");
+        }
+
+        public void FireSomeone()
+        {
+            Console.WriteLine("You're fired!");
+        }
+    }
+}
---
+++ LSPDemo/DemoLibrary/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Manager : Employee, IManager
+    {
+        public override void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 19.75m;
+
+            Salary = baseAmount + (rank * 4);
+        }
+        public void GeneratePerformanceReview()
+        {
+            Console.WriteLine("I'm reviewing a direct report's performance.");
+        }
+    }
+}
