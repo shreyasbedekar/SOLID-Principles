0c07e63efee4c9d08023a3603082ef9b4938c6d3
Shreyas Bedekar <bedesn01@pfw.edu>
2024-11-19 18:40:15 -0500

    ISP

---
+++ ISPDemo/DemoLibrary/DigitalMovie/DigitalMovie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.DigitalMovie
+{
+    public class DigitalMovie : IBorrowableDVD
+    {
+        public string LibraryId { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public string Borrower { get; set; }
+        public int CheckOutDurationInDays { get; set; } = 14;
+        public string Title { get; set; }
+        public List<string> Actors { get; set; }
+        public int RuntimeInMinutes { get; set; }
+        public void CheckIn()
+        {
+            Borrower = "";
+        }
+
+        public void CheckOut(string borrower)
+        {
+            Borrower = borrower;
+            BorrowDate = DateTime.Now;
+        }
+
+        public DateTime GetDueDate()
+        {
+            return BorrowDate.AddDays(CheckOutDurationInDays);
+        }
+    }
+}
